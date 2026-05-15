b896c82bf0fdebc83e8ac34f3ff97f451368d713
modulomedito <chcchc1995@outlook.com>
2026-05-15 17:08:13 +0800

    🐞 fix(macro):[#]fix error macro names

--- src/crypto_sha256.h
+++ src/crypto_sha256.h
@@ -26,9 +26,9 @@ extern "C" {
 //==================================================================================================
 // PUBLIC DEFINE
 //==================================================================================================
-#define CRYPTO_SHA__MAJOR_VERSION (0)
-#define CRYPTO_SHA__MINOR_VERSION (1)
-#define CRYPTO_SHA__PATCH_VERSION (0)
+#define CRYPTO_SHA256__MAJOR_VERSION (0)
+#define CRYPTO_SHA256__MINOR_VERSION (1)
+#define CRYPTO_SHA256__PATCH_VERSION (0)
 
 #define CRYPTO_SHA256__BLOCK_U8_SIZE (32)
 
